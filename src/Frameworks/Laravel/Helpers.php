6168d5a56c346cf74147a8f0517b7e01e2d33aeb
Mario Bašić <mario@laravelista.hr>
2021-03-25 12:30:34 +0100

    Uses App::make instead of App::resolve in Laravel global helpers.

--- src/Frameworks/Laravel/Helpers.php
+++ src/Frameworks/Laravel/Helpers.php
@@ -10,28 +10,28 @@ use Laravelista\Ekko\Frameworks\Laravel\Ekko;
 if (!function_exists('isActiveRoute')) {
     function isActiveRoute(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActiveRoute($input, $output);
+        return App::make(Ekko::class)->isActiveRoute($input, $output);
     }
 }
 
 if (!function_exists('is_active_route')) {
     function is_active_route(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActiveRoute($input, $output);
+        return App::make(Ekko::class)->isActiveRoute($input, $output);
     }
 }
 
 if (!function_exists('areActiveRoutes')) {
     function areActiveRoutes(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActiveRoute($input, $output);
+        return App::make(Ekko::class)->isActiveRoute($input, $output);
     }
 }
 
 if (!function_exists('are_active_routes')) {
     function are_active_routes(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActiveRoute($input, $output);
+        return App::make(Ekko::class)->isActiveRoute($input, $output);
     }
 }
 
@@ -45,35 +45,35 @@ if (!function_exists('is_active')) {
      */
     function is_active(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('isActiveURL')) {
     function isActiveURL(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('is_active_url')) {
     function is_active_url(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('areActiveURLs')) {
     function areActiveURLs(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('are_active_urls')) {
     function are_active_urls(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
@@ -84,27 +84,27 @@ if (!function_exists('are_active_urls')) {
 if (!function_exists('isActiveMatch')) {
     function isActiveMatch(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('is_active_match')) {
     function is_active_match(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('AreActiveMatches')) {
     function AreActiveMatches(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
 
 if (!function_exists('are_active_matches')) {
     function are_active_matches(array|string $input, mixed $output = null): mixed
     {
-        return App::resolve(Ekko::class)->isActive($input, $output);
+        return App::make(Ekko::class)->isActive($input, $output);
     }
 }
